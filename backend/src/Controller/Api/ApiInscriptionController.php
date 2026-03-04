b9bb8946f9d735458117f1c6920fbe2b6f9d1254
ENFANTSDUPAYS <adrien.leclere@orange.fr>
2026-03-04 09:28:04 +0100

    feat(serializer): add serializer for project

--- backend/src/Controller/Api/ApiInscriptionController.php
+++ backend/src/Controller/Api/ApiInscriptionController.php
@@ -11,7 +11,7 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Route;
 
 
-class InscriptionController extends AbstractController
+class ApiInscriptionController extends AbstractController
 {
     #[Route(path: '/api/inscription', name: 'api_inscription', methods: ['POST'])]
     public function inscription(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): JsonResponse
@@ -30,23 +30,5 @@ class InscriptionController extends AbstractController
         $entityManager->flush();
 
         return new JsonResponse(['message' => 'Inscription réussie.'], 201);
-    }
-
-    #[Route(path: '/api/connexion', name: 'api_connexion', methods: ['POST'])]
-    public function connexion(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): JsonResponse
-    {
-        $data = json_decode($request->getContent(), true);
-
-        if (empty($data['email']) || empty($data['mot_de_passe'])) {
-            return new JsonResponse(['message' => 'Email et mot de passe sont requis.'], 400);
-        }
-
-        $utilisateur = $entityManager->getRepository(Utilisateur::class)->findOneBy(['email' => $data['email']]);
-
-        if (!$utilisateur || !$passwordHasher->isPasswordValid($utilisateur, $data['mot_de_passe'])) {
-            return new JsonResponse(['message' => 'Email ou mot de passe incorrect.'], 401);
-        }
-
-        return new JsonResponse(['message' => 'Connexion réussie.'], 200);
     }
 }
