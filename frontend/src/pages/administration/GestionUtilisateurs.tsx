268122adf7f01acdf5215c2dd05560adf7ddcbaa
Adrien <adrien.leclere@orange.fr>
2026-03-20 17:26:08 +0100

    style(logo): edit logo filter

--- frontend/src/pages/administration/GestionUtilisateurs.tsx
+++ frontend/src/pages/administration/GestionUtilisateurs.tsx
@@ -222,9 +222,7 @@ export default function GestionUtilisateurs() {
                         {aDesFiltresActifs ? (
                             <img src="/src/assets/layers_clear.svg" alt="" className="w-5 h-5" />
                         ) : (
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round">
-                                <polygon points="22 3 2 3 10 12.46 10 19 14 21 14 12.46 22 3"/>
-                            </svg>
+                            <img src="/src/assets/filter_alt.svg" alt="" className="w-5 h-5" />
                         )}
                     </button>
                 </div>
